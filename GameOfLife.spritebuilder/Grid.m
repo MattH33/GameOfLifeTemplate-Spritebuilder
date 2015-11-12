2b54e0e8c197fb6a904c6b9b23a778f27019afeb
Matt H <Matthew@Matts-Air.cg.shawcable.net>
2015-11-11 21:11:47 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -91,4 +91,98 @@ static const int GRID_COLUMNS = 10;
     return _gridArray[row][column];
 }
 
+- (void)evolveStep {
+    //update each Creature's neighbor count
+    [self countNeighbors];
+    
+    //update each Creature's state
+    [self updateCreatures];
+    
+    //update the generation so the label's text will display the correct generation
+    _generation++;
+}
+
+- (void)countNeighbors{
+    // iterate through the rows
+    for (int i = 0; i < [_gridArray count]; i++)
+    {
+        // iterate through all the columns for a given row
+        for (int j = 0; j < [_gridArray[i] count]; j++)
+        {
+            // access the creature in the cell that corresponds to the current row/column
+            Creature *currentCreature = _gridArray[i][j];
+            
+            // remember that every creature has a 'livingNeighbors' property that we created earlier
+            currentCreature.livingNeighbors = 0;
+            
+            // now examine every cell around the current one
+            
+            // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
+            for (int x = (i-1); x <= (i+1); x++)
+            {
+                // go through the column to the left of the current cell, the column the cell is in, and the column to the right of the current cell
+                for (int y = (j-1); y <= (j+1); y++)
+                {
+                    // check that the cell we're checking isn't off the screen
+                    BOOL isIndexValid;
+                    isIndexValid = [self isIndexValidForX:x andY:y];
+                    
+                    // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
+                    if (!((x == i) && (y == j)) && isIndexValid)
+                    {
+                        Creature *neighbor = _gridArray[x][y];
+                        if (neighbor.isAlive)
+                        {
+                            currentCreature.livingNeighbors += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+- (void)updateCreatures{
+    
+    // keeps the total number of current creatures that are alive
+    _totalAlive = 0;
+    
+    // iterate through the rows
+    for (int i = 0; i < [_gridArray count]; i++) {
+        
+        // iterate through all the columns for a given row
+        for (int j = 0; j < [_gridArray[i] count]; j++) {
+            
+            // access the creature in the cell that corresponds to the current row/column
+            Creature *currentCreature = _gridArray[i][j];
+            
+            // check if the Creature's livingNeighbors property is set to 3
+            if (currentCreature.livingNeighbors == 3) {
+                
+                // If it is, that means it has 3 live neighbors so you want to set its isAlive property to TRUE
+                currentCreature.isAlive = YES;
+                
+            // check if the Creature has less than or equal to 1 living neighbors or more than or equal to 4
+            } else if ( (currentCreature.livingNeighbors <= 1) || (currentCreature.livingNeighbors >= 4)) {
+                // If either are true, set the Creature's isAlive property to FALSE.
+                currentCreature.isAlive = NO;
+            }
+            // if a creature is alive, increase the totalAlive property
+            if (currentCreature.isAlive) {
+                    _totalAlive++;
+            }
+        }
+    }
+}
+
+- (BOOL)isIndexValidForX:(int)x andY:(int)y
+{
+    BOOL isIndexValid = YES;
+    if(x < 0 || y < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS)
+    {
+        isIndexValid = NO;
+    }
+    return isIndexValid;
+}
+
 @end
--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -7,7 +7,44 @@
 //
 
 #import "MainScene.h"
+#import "Grid.h"
 
-@implementation MainScene
+@implementation MainScene {
+    Grid *_grid;
+    CCTimer *_timer;
+    CCLabelTTF *_generationLabel;
+    CCLabelTTF *_populationLabel;
+}
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        _timer = [[CCTimer alloc] init];
+    }
+    
+    return self;
+}
+
+//this tells the game to call the method called 'step' every half second.
+- (void)play
+{
+    [self schedule:@selector(step) interval:0.5f];
+}
+
+//this tells the game to call a stop the method called 'step' when the pause button is tapped.
+- (void)pause
+{
+    [self unschedule:@selector(step)];
+}
+
+// this method will get called every half second when you hit the play button and will stop getting called when you hit the pause button. update the stats and gamestate (generation, alive/dead creatures)
+- (void)step
+{
+    [_grid evolveStep];
+    _generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
+    _populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
+}
 
 @end
